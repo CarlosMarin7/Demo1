f0f5ca0383351e5b09acd4a84c21d31742a305bf
Carlos Marin <carlos.marinva@cun.edo.co>
2023-03-15 21:03:34 -0500

    Version incial

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Hola mundo 2")
+var contador = 0;
+var myButton = document.getElementById("boton");
+var myText = document.getElementById("texto");
+myButton.onclick = function(){
+    contador++;
+    if (contador > 9) {
+        contador = 0
+    }
+    myText.textContent = contador;   
+}
